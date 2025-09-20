be5e557b3a08315f28d6b56380c18aee6ab8e766
PuttaPramod <pramodkumarputta06@gmail.com>
2025-09-20 16:26:13 +0530

    changed aos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import AOS from 'aos';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'] // keep your own CSS
 })
-export class FooterComponent implements OnInit {
-  ngOnInit() {
-    AOS.init({
-      duration: 1000,
-      once: true
-    });
-  }
+export class FooterComponent {
+  
 }
