b0f5cf7cb2332c396cd96e783838d5c598d182c7
PuttaPramod <pramodkumarputta06@gmail.com.com>
2026-03-17 14:30:19 +0530

    backup

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,5 @@
-
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import emailjs from '@emailjs/browser';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -8,7 +7,7 @@ import AOS from 'aos';
 
 @Component({
   selector: 'app-contact',
-  imports:[CommonModule,FormsModule,ReactiveFormsModule,HttpClientModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
@@ -55,25 +54,25 @@ export class ContactComponent {
       'template_email',     // e.g., template_ab12cd3
       templateParams
     )
-    .then(() => {
-      this.isSubmitted = true;
-      this.submitMessage = 'Thank you! Your message has been sent successfully.';
-      this.contactForm.reset();
-    })
-    .catch((error) => {
-      console.error('EmailJS Error:', error.text);
-      this.submitMessage = 'Sorry, something went wrong. Please try again later.';
-    })
-    .finally(() => {
-      this.isSubmitting = false;
-      setTimeout(() => {
-        this.isSubmitted = false;
-        this.submitMessage = '';
-      }, 5000);
-    });
+      .then(() => {
+        this.isSubmitted = true;
+        this.submitMessage = 'Thank you! Your message has been sent successfully.';
+        this.contactForm.reset();
+      })
+      .catch((error) => {
+        console.error('EmailJS Error:', error.text);
+        this.submitMessage = 'Sorry, something went wrong. Please try again later.';
+      })
+      .finally(() => {
+        this.isSubmitting = false;
+        setTimeout(() => {
+          this.isSubmitted = false;
+          this.submitMessage = '';
+        }, 5000);
+      });
   }
-  
-   ngOnInit(): void {
+
+  ngOnInit(): void {
     // Initialize form
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
@@ -88,8 +87,8 @@ export class ContactComponent {
       once: true,
       offset: 100
     });
-    
+
     // Refresh AOS after view init
     setTimeout(() => AOS.refresh(), 100);
   }
-  }
+}
